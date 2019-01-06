43daad14ca864fe69e561fe3da1d4a82950f24b1
Subin Shrestha <subeenshrestha@hotmail.com>
2019-01-06 19:25:54 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 21, 2019 18:03:26",
+	date: "January 21, 2019 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
